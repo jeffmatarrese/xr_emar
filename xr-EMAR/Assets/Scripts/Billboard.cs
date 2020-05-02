a9344727624e244a31aa04b20d129b52756b5bf4
Jeff Matarrese <jeff.matarrese@gmail.com>
2020-05-01 21:27:32 -0700

    Initial Commit
    Initial commit of basic Unity scene for xr-emar. Includes very rough initial flow.
    - Set up for WebGL
    - Ensure WebXR extension is installed
    - Build and run will automatically launch local server.

---
+++ xr-EMAR/Assets/Scripts/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Camera mainCamera;
+    
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward, mainCamera.transform.rotation * Vector3.up);
+    }
+}
---
+++ xr-EMAR/Assets/Scripts/InteractionSequence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionSequence : MonoBehaviour
+{
+
+
+    // Get all of the elements
+    // get first text
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
